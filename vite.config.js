b71100c2149885204cdd1133cbfd1fdcff5ee19b
Zihong <Zihongchen0227@gmail.com>
2026-06-16 23:30:28 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/tsawebmaster2026"
+  base: '/tsawebmaster2026/',
 })
